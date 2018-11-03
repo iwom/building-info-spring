e92251fccdea42151de34497e41579263191cc86
iwom <17498068+iwom@users.noreply.github.com>
2018-11-03 10:07:11 +0100

    [1] POST @RequestMapping update

--- backend-builder/src/main/java/com/builder/demo/controller/BuildingController.java
+++ backend-builder/src/main/java/com/builder/demo/controller/BuildingController.java
@@ -2,6 +2,7 @@ package com.builder.demo.controller;
 
 import com.builder.demo.service.impl.BuildingServiceImpl;
 import com.builder.demo.shared.dto.BuildingDto;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,8 @@ public class BuildingController {
     BuildingServiceImpl buildingService;
 
     @PostMapping
-    @RequestMapping(path = "/buildings")
+    @RequestMapping(path = "/buildings",
+                    consumes = {MediaType.APPLICATION_JSON_VALUE }, produces = {MediaType.APPLICATION_JSON_VALUE })
     public BuildingDto createBuilding(@RequestBody BuildingDto buildingDto) {
         return buildingService.createBuilding(buildingDto);
     }
