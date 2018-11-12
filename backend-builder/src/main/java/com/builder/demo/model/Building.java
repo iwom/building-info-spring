e54f06bbe7a9e416d929acee7e59528c8adc2002
iwom <17498068+iwom@users.noreply.github.com>
2018-11-12 15:28:20 +0100

    [7] Add javadoc for existsing services

--- backend-builder/src/main/java/com/builder/demo/model/Building.java
+++ backend-builder/src/main/java/com/builder/demo/model/Building.java
@@ -8,23 +8,45 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * BuidlingEntity is the major abstraction unit in this project.
+ * The entity holds information about all its children
+ * @see com.builder.demo.model.Floor
+ * @see com.builder.demo.model.Room
+ * @author iwom
+ */
 @Entity
 @Table(name = "buildings")
 @Getter
 @Setter
 @ToString
 public class Building implements Serializable {
+    /**
+     * UID for serialization purposes
+     */
     private static final long serialVersionUID = 5313493413859894401L;
+    /**
+     * Unique id which allows identification of a building with REST API
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
     private Long id;
+    /**
+     * User chosen name of the building
+     */
     private String name;
 
+    /**
+     * Collection of floors associated with the building
+     */
     @OneToMany(mappedBy = "building", cascade = CascadeType.ALL)
     @Getter
     private List<Floor> floorList;
 
+    /**
+     * Collection of rooms associated with the building
+     */
     @OneToMany(mappedBy = "building", cascade = CascadeType.ALL)
     private List<Room> roomList;
 }
--- backend-builder/src/main/java/com/builder/demo/service/RoomService.java
+++ backend-builder/src/main/java/com/builder/demo/service/RoomService.java
@@ -2,6 +2,17 @@ package com.builder.demo.service;
 
 import com.builder.demo.shared.dto.RoomDto;
 
+/**
+ * RoomService - interface allowing CRUD operations on {@link com.builder.demo.shared.dto.RoomDto}
+ * This service uses {@link com.builder.demo.repostitory.RoomRepository} to communicate with mysql database
+ * @author iwom
+ */
 public interface RoomService {
+    /**
+     * @param roomDto RoomDto to be created and persisted to database as {@link com.builder.demo.model.Room}
+     * @param buildingId parent id enabling to establish relationship between {@link com.builder.demo.model.Room} and {@link com.builder.demo.model.Building}
+     * @param floorId parent id enabling to establish relationship between {@link com.builder.demo.model.Room} and {@link com.builder.demo.model.Floor}
+     * @return {@link com.builder.demo.shared.dto.RoomDto} if the object was succesfully created, 4xx error otherwise.
+     */
     RoomDto createRoom(RoomDto roomDto, Long buildingId, Long floorId);
 }
